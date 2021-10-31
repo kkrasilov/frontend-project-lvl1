69366d17736f05a070ea8c1c951856b92374f2b0
kkrasilov <kkrasilov@gmail.com>
2021-10-31 15:13:14 +0500

    Refactorin brain-even game

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -3,9 +3,12 @@ import run from '../index.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (number) => (number % 2 === 0);
+
 const questionAndAnswer = () => {
-  const question = _.random(100);
-  const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+  const number = _.random(100);
+  const question = String(number);
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
 
   return { question, correctAnswer };
 };
